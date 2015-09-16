de55add74d237c0a22eb0f5c99dbfa65bee4c8d8
Syed Monowar Hossain <monowar.hossain@gmail.com>
2015-09-16 13:55:16 -0500

    Initial commit

---
+++ app/src/main/java/org/md2k/apps/ActivitymCerebrumApp.java
@@ -0,0 +1,44 @@
+package org.md2k.apps;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import org.md2k.utilities.Report.Log;
+
+
+public class ActivitymCerebrumApp extends Activity {
+    public static final String TAG=ActivitymCerebrumApp.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate()");
+        setContentView(R.layout.activity_mcerebrumapp);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_settings, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            Intent intent = new Intent(this, ActivitymCerebrumAppSettings.class);
+            startActivity(intent);
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
